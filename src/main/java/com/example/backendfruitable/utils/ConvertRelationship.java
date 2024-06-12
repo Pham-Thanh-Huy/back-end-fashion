d4605a6837cb22730fa7c919c0e31d13b1203e5b
Pham Thanh Huy <huypham3062k3@gmail.com>
2024-06-12 16:18:05 +0700

    commit

--- src/main/java/com/example/backendfruitable/utils/ConvertRelationship.java
+++ src/main/java/com/example/backendfruitable/utils/ConvertRelationship.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ConvertRelationship {
@@ -30,22 +31,22 @@ public class ConvertRelationship {
         return imageDTOList;
     }
 
-    public List<InventoryDTO> convertToInventoryDTOList(List<Inventory> inventoryList){
-      List<InventoryDTO> inventoryDTOList = new ArrayList<>();
-      for(Inventory inventory : inventoryList){
-          InventoryDTO inventoryDTO = new InventoryDTO();
-          inventoryDTO.setInventoryId(inventory.getInventoryId());
-          inventoryDTO.setQuantity(inventory.getQuantity());
-          inventoryDTO.setProductColor(convertToProductColorDTO(inventory.getProductColor()));
-          inventoryDTO.setProductSize(convertToProductSizeDTO(inventory.getProductSize()));
-          inventoryDTOList.add(inventoryDTO);
-      }
+    public List<InventoryDTO> convertToInventoryDTOList(List<Inventory> inventoryList) {
+        List<InventoryDTO> inventoryDTOList = new ArrayList<>();
+        for (Inventory inventory : inventoryList) {
+            InventoryDTO inventoryDTO = new InventoryDTO();
+            inventoryDTO.setInventoryId(inventory.getInventoryId());
+            inventoryDTO.setQuantity(inventory.getQuantity());
+            inventoryDTO.setProductColor(convertToProductColorDTO(inventory.getProductColor()));
+            inventoryDTO.setProductSize(convertToProductSizeDTO(inventory.getProductSize()));
+            inventoryDTOList.add(inventoryDTO);
+        }
         return inventoryDTOList;
     }
 
-    public List<Inventory>  convertToInventoryList(List<InventoryDTO> inventoryDTOList){
+    public List<Inventory> convertToInventoryList(List<InventoryDTO> inventoryDTOList) {
         List<Inventory> inventoryList = new ArrayList<>();
-        for(InventoryDTO inventoryDTO : inventoryDTOList){
+        for (InventoryDTO inventoryDTO : inventoryDTOList) {
             Inventory inventory = new Inventory();
             inventory.setInventoryId(inventoryDTO.getInventoryId());
             inventory.setQuantity(inventoryDTO.getQuantity());
@@ -57,35 +58,35 @@ public class ConvertRelationship {
         return inventoryList;
     }
 
-    public ProductColorDTO convertToProductColorDTO(ProductColor productColor){
+    public ProductColorDTO convertToProductColorDTO(ProductColor productColor) {
         ProductColorDTO productColorDTO = new ProductColorDTO();
         productColorDTO.setProductColorId(productColor.getProductColorId());
         productColorDTO.setColorName(productColor.getColorName());
         return productColorDTO;
     }
 
-    public ProductSizeDTO convertToProductSizeDTO(ProductSize productSize){
+    public ProductSizeDTO convertToProductSizeDTO(ProductSize productSize) {
         ProductSizeDTO productSizeDTO = new ProductSizeDTO();
         productSizeDTO.setProductSizeId(productSize.getProductSizeId());
         productSizeDTO.setSizeName(productSize.getSizeName());
         return productSizeDTO;
     }
 
-    public ProductColor convertToProductColor(ProductColorDTO productColorDTO){
+    public ProductColor convertToProductColor(ProductColorDTO productColorDTO) {
         ProductColor productColor = new ProductColor();
         productColor.setProductColorId(productColorDTO.getProductColorId());
         productColor.setColorName(productColorDTO.getColorName());
         return productColor;
     }
 
-    public ProductSize convertToProductSize(ProductSizeDTO productSizeDTO){
+    public ProductSize convertToProductSize(ProductSizeDTO productSizeDTO) {
         ProductSize productSize = new ProductSize();
         productSize.setProductSizeId(productSizeDTO.getProductSizeId());
         productSize.setSizeName(productSizeDTO.getSizeName());
         return productSize;
     }
 
-    public UserDTO convertToUserDTO(User user){
+    public UserDTO convertToUserDTO(User user) {
         UserDTO userDTO = new UserDTO();
         userDTO.setFirstName(user.getFirstName());
         userDTO.setLastName(user.getLastname());
@@ -93,24 +94,24 @@ public class ConvertRelationship {
         userDTO.setEmail(user.getEmail());
         userDTO.setAddress(user.getAddress());
         userDTO.setPhoneNumber(user.getPhoneNumber());
-     // lấy ảnh từ minio
-      try{
-          String object = user.getUserImage();
-          String imageUrl = minioClient.getPresignedObjectUrl(
-                  GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(object).build()
-          );
-          userDTO.setUserImage(object);
-          userDTO.setImageUrl(imageUrl);
-      }catch (Exception e){
-          throw new RuntimeException(e.getMessage());
-      }
-        return  userDTO;
+        // lấy ảnh từ minio
+        try {
+            String object = user.getUserImage();
+            String imageUrl = minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(object).build()
+            );
+            userDTO.setUserImage(object);
+            userDTO.setImageUrl(imageUrl);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return userDTO;
     }
 
 
-    public List<ImageProduct> convertToImageList(List<ImageDTO> imageDTOList){
+    public List<ImageProduct> convertToImageList(List<ImageDTO> imageDTOList) {
         List<ImageProduct> imageProductList = new ArrayList<>();
-        for(ImageDTO imageDTO : imageDTOList){
+        for (ImageDTO imageDTO : imageDTOList) {
             ImageProduct imageProduct = new ImageProduct();
             imageProduct.setImageProduct(imageDTO.getImageProduct());
             imageProductList.add(imageProduct);
@@ -118,7 +119,7 @@ public class ConvertRelationship {
         return imageProductList;
     }
 
-    public Product convertToProduct(ProductDTO productDTO){
+    public Product convertToProduct(ProductDTO productDTO) {
         Product product = new Product();
         product.setProductId(productDTO.getProductId());
         product.setProductName(productDTO.getProductName());
@@ -129,7 +130,7 @@ public class ConvertRelationship {
         product.setProductDescription(productDTO.getProductDescription());
         product.setOutstanding(productDTO.getOutstanding());
         product.setCreatedAt(productDTO.getCreatedAt());
-        return  product;
+        return product;
     }
 
     public ProductDTO convertToProductDTO(Product product) {
@@ -142,14 +143,38 @@ public class ConvertRelationship {
         productDTO.setProductDescription(product.getProductDescription());
         productDTO.setOutstanding(product.getOutstanding());
         productDTO.setCreatedAt(product.getCreatedAt());
+        List<ImageDTO> imageList = product.getImageList()
+                .stream()
+                .map(imageProduct -> {
+                    ImageDTO imageDTO = new ImageDTO();
+                    try {
+                        String imageUrl = minioClient.getPresignedObjectUrl(
+                                GetPresignedObjectUrlArgs
+                                        .builder()
+                                        .method(Method.GET)
+                                        .bucket(bucketName)
+                                        .object(imageProduct.getImageProduct())
+                                        .build()
+
+                        );
+                        imageDTO.setImageProduct(imageProduct.getImageProduct());
+                        imageDTO.setImageUrl(imageUrl);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e.getMessage());
+                    }
+                    return imageDTO;
+                })
+                .collect(Collectors.toList());
+        productDTO.setImageList(imageList);
+
+
         return productDTO;
     }
 
 
-
-    public List<AuthorizeDTO> converToAuthorizeDTOList(List<Authorize> authorizeList){
+    public List<AuthorizeDTO> converToAuthorizeDTOList(List<Authorize> authorizeList) {
         List<AuthorizeDTO> authorizeDTOList = new ArrayList<>();
-        for(Authorize authorize : authorizeList){
+        for (Authorize authorize : authorizeList) {
             AuthorizeDTO authorizeDTO = new AuthorizeDTO();
             authorizeDTO.setAuthorizeId(authorize.getAuthorizeId());
             authorizeDTO.setAuthorizeName(authorize.getAuthorizeName());
@@ -159,7 +184,7 @@ public class ConvertRelationship {
     }
 
 
-    public CategoryProductDTO convertToCategoryProductDTO(CategoryProduct categoryProduct){
+    public CategoryProductDTO convertToCategoryProductDTO(CategoryProduct categoryProduct) {
         CategoryProductDTO categoryProductDTO = new CategoryProductDTO();
         categoryProductDTO.setCategoryId(categoryProduct.getCategoryId());
         categoryProductDTO.setCategoryName(categoryProduct.getCategoryName());
@@ -167,7 +192,7 @@ public class ConvertRelationship {
         return categoryProductDTO;
     }
 
-    public CategoryPostDTO convertToCategoryPostDTO(CategoryPost categoryPost){
+    public CategoryPostDTO convertToCategoryPostDTO(CategoryPost categoryPost) {
         CategoryPostDTO categoryPostDTO = new CategoryPostDTO();
         categoryPostDTO.setCategoryId(categoryPost.getCategoryId());
         categoryPostDTO.setCategoryName(categoryPost.getCategoryName());
@@ -175,7 +200,7 @@ public class ConvertRelationship {
         return categoryPostDTO;
     }
 
-    public DeliveryMethodDTO convertToDeliveryMethodDTO(DeliveryMethod deliveryMethod){
+    public DeliveryMethodDTO convertToDeliveryMethodDTO(DeliveryMethod deliveryMethod) {
         DeliveryMethodDTO deliveryMethodDTO = new DeliveryMethodDTO();
         deliveryMethodDTO.setDeliveryId(deliveryMethod.getDeliveryId());
         deliveryMethodDTO.setName(deliveryMethod.getName());
@@ -184,7 +209,7 @@ public class ConvertRelationship {
         return deliveryMethodDTO;
     }
 
-    public PaymentMethodDTO convertToPaymentMethodDTO(PaymentMethod paymentMethod){
+    public PaymentMethodDTO convertToPaymentMethodDTO(PaymentMethod paymentMethod) {
         PaymentMethodDTO paymentMethodDTO = new PaymentMethodDTO();
         paymentMethodDTO.setPaymentId(paymentMethod.getPaymentId());
         paymentMethodDTO.setPaymentName(paymentMethod.getPaymentName());
